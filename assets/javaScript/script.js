8cdc75de5fb3c563745226b208c30576b0ed4d1b
Sam Jones <samejones2018@gmail.com>
2023-02-20 12:13:55 -0800

    created push notifications and am working on confirmation

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -16,4 +16,33 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);{}
+
+function generatePassword() {
+
+while(true) {
+var passwordLength = prompt("Choose a password length between 8 and 128 characters.");
+
+var bigCase = confirm('Include uppercase letters ("Ok" to confirm, "Cancel" to deny)?');
+
+var smallCase = confirm('Include lowercase letters ("Ok" to confirm, "Cancel" to deny)?');
+
+var numeric = confirm('Include numbers ("Ok" to confirm, "Cancel" to deny)?');
+
+var special = confirm('Include special characters ("Ok" to confirm, "Cancel" to deny)?');
+
+if (bigCase === true && smallCase === true && numeric === true && special === true) {
+   alert('Uppercase, Lowercase, Numbers and Special Characters will be added!');
+}else if (bigCase === true && smallCase === true && numeric === true && special === false) {
+   alert('Uppercase, Lowercase, and Numbers have been added!');
+  }else if(bigCase === true && smallCase === true && numeric === false && special === false){ 
+    alert('Uppercase and Lowercase will be added!');
+} else if(bigCase === true && smallCase === false && numeric === false && special === false){
+  alert('Uppercase will be added!')
+   } else {
+    alert('Not enough options selected. Try Again.')
+   break; 
+  }
+  
+  }
+ }
