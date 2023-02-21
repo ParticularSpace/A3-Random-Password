049cd13c46e249a2fd0219b7b433bb700b1285b4
Sam Jones <samejones2018@gmail.com>
2023-02-20 19:29:58 -0800

    created variables,checked selected variables, confirmed choices, displayed random password

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
@@ -12,37 +11,54 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{}
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+var passwordSize = prompt("Choose a password length between 8 and 128 characters.");
+if(passwordSize < 8 || passwordSize > 128){
+  alert('Password length must be between 8 and 128 characters.');
+  return '';
+}
 
-while(true) {
-var passwordLength = prompt("Choose a password length between 8 and 128 characters.");
+alert('You chose ' + passwordSize + ' characters!');
 
-var bigCase = confirm('Include uppercase letters ("Ok" to confirm, "Cancel" to deny)?');
+  var bigCase = confirm('Include uppercase letters ("Ok" to confirm, "Cancel" to deny)?');
+  var smallCase = confirm('Include lowercase letters ("Ok" to confirm, "Cancel" to deny)?');
+  var numeric = confirm('Include numbers ("Ok" to confirm, "Cancel" to deny)?');
+  var special = confirm('Include special characters ("Ok" to confirm, "Cancel" to deny)?');
 
-var smallCase = confirm('Include lowercase letters ("Ok" to confirm, "Cancel" to deny)?');
+if (bigCase === false && smallCase === false && numeric === false && special === false) {
+   alert('Must select at least ONE option.');
+   return '';
+   }
 
-var numeric = confirm('Include numbers ("Ok" to confirm, "Cancel" to deny)?');
+var userChoiceCharacter = "";
 
-var special = confirm('Include special characters ("Ok" to confirm, "Cancel" to deny)?');
-
-if (bigCase === true && smallCase === true && numeric === true && special === true) {
-   alert('Uppercase, Lowercase, Numbers and Special Characters will be added!');
-}else if (bigCase === true && smallCase === true && numeric === true && special === false) {
-   alert('Uppercase, Lowercase, and Numbers have been added!');
-  }else if(bigCase === true && smallCase === true && numeric === false && special === false){ 
-    alert('Uppercase and Lowercase will be added!');
-} else if(bigCase === true && smallCase === false && numeric === false && special === false){
-  alert('Uppercase will be added!')
-   } else {
-    alert('Not enough options selected. Try Again.')
-   break; 
+  if (bigCase === true) {
+    userChoiceCharacter += upperCase;
+    alert('Uppercase has been added!');
   }
-  
+  if (smallCase === true) {
+    userChoiceCharacter += lowerCase;
+    alert('Lowercase has been added!');
   }
+  if (numeric === true) {
+    userChoiceCharacter += numbers;
+    alert('Numbers have been added!');
+  }
+  if (special === true) {
+    userChoiceCharacter += specialCharacters;
+    alert('Special characters have been added!');
+  }
+
+  var password = "";
+    for (var i = 0; i < passwordSize; i++) {
+      var randomPin = Math.floor(Math.random() * userChoiceCharacter.length);
+      password += userChoiceCharacter.substring(randomPin, randomPin+1);
+    }
+  return password;
  }
+
