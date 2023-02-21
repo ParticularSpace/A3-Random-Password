70115d5f14c03028c279b9c486b38e5900c2f7d2
Sam Jones <samejones2018@gmail.com>
2023-02-20 21:01:36 -0800

    commited my code while cleaning semantics

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code created variables for elements
 var generateBtn = document.querySelector("#generate");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -16,27 +16,28 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Checks size requested and if it fits in the requirements
 function generatePassword() {
 var passwordSize = prompt("Choose a password length between 8 and 128 characters.");
 if(passwordSize < 8 || passwordSize > 128){
   alert('Password length must be between 8 and 128 characters.');
   return '';
 }
-
+//Alerts to chosen password length
 alert('You chose ' + passwordSize + ' characters!');
-
+//Confirm choices and assigns values to their corresponding variables
   var bigCase = confirm('Include uppercase letters ("Ok" to confirm, "Cancel" to deny)?');
   var smallCase = confirm('Include lowercase letters ("Ok" to confirm, "Cancel" to deny)?');
   var numeric = confirm('Include numbers ("Ok" to confirm, "Cancel" to deny)?');
   var special = confirm('Include special characters ("Ok" to confirm, "Cancel" to deny)?');
-
+//If all section are left empty reset
 if (bigCase === false && smallCase === false && numeric === false && special === false) {
    alert('Must select at least ONE option.');
    return '';
    }
-
+//Created a new variable to hold each selected option based off of the true or false value
 var userChoiceCharacter = "";
-
+//Check if the option is selected
   if (bigCase === true) {
     userChoiceCharacter += upperCase;
     alert('Uppercase has been added!');
@@ -53,12 +54,16 @@ var userChoiceCharacter = "";
     userChoiceCharacter += specialCharacters;
     alert('Special characters have been added!');
   }
-
+//Create our password variable that is going to put out a string
   var password = "";
-    for (var i = 0; i < passwordSize; i++) {
-      var randomPin = Math.floor(Math.random() * userChoiceCharacter.length);
-      password += userChoiceCharacter.substring(randomPin, randomPin+1);
+  //The next for loop will be the password generator based on the user choice character thus passwordSize as length
+  //we choose a random number and round down to a whole number, this number picks a character from userChoiceCharacter 
+  //using the substring function we single out one character from userChoiceCharacter and add it to the password
+  for (var i = 0; i < passwordSize; i++) {
+      var randomSelector = Math.floor(Math.random() * userChoiceCharacter.length);
+      password += userChoiceCharacter.substring(randomSelector, randomSelector+1);
     }
+    //our final return statement calling our random password variable
   return password;
  }
 
